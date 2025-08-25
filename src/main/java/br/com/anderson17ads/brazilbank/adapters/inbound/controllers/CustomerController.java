45e55d7fabfaf5872d76232b1df1e48c985339ca
Anderson de Souza <anderson17ads@hotmail.com>
2025-08-24 23:20:39 -0300

    Refactoring user nomenclature for customer

--- src/main/java/br/com/anderson17ads/brazilbank/adapters/inbound/controllers/CustomerController.java
+++ src/main/java/br/com/anderson17ads/brazilbank/adapters/inbound/controllers/CustomerController.java
@@ -3,19 +3,19 @@ package br.com.anderson17ads.brazilbank.adapters.inbound.controllers;
 import com.fasterxml.jackson.databind.JsonNode;
 import org.springframework.web.bind.annotation.*;
 
-@RequestMapping("/user")
+@RequestMapping("/customer")
 @RestController
-public class UserController {
+public class CustomerController {
     @GetMapping
     public String list(@RequestParam(value = "filter", required = false) String filter) {
         return (filter == null)
-            ? "User List without param filter"
-            : "User List with param filter " + filter;
+            ? "Customer List without param filter"
+            : "Customer List with param filter " + filter;
     }
 
     @GetMapping("/{id}")
     public String detail(@PathVariable("id") String id) {
-        return "User detail " + id;
+        return "Customer detail " + id;
     }
 
     @PostMapping
