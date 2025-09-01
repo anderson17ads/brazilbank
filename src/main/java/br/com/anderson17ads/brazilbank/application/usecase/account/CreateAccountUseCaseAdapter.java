7dc75238f265d66f07f61a0fc14da30628b782ba
Anderson de Souza <anderson17ads@hotmail.com>
2025-08-31 23:13:26 -0300

    Implementation accountNumberPolicy for account create

--- src/main/java/br/com/anderson17ads/brazilbank/application/usecase/account/CreateAccountUseCaseAdapter.java
+++ src/main/java/br/com/anderson17ads/brazilbank/application/usecase/account/CreateAccountUseCaseAdapter.java
@@ -4,19 +4,22 @@ import br.com.anderson17ads.brazilbank.domain.account.Account;
 import br.com.anderson17ads.brazilbank.domain.account.AccountFactory;
 import br.com.anderson17ads.brazilbank.domain.account.AccountRepository;
 import br.com.anderson17ads.brazilbank.application.command.account.CreateAccountCommand;
+import br.com.anderson17ads.brazilbank.domain.account.policy.AccountNumberPolicy;
 
 public class CreateAccountUseCaseAdapter implements CreateAccountUseCase {
     private final AccountRepository accountRepository;
     private final AccountFactory accountFactory;
+    private final AccountNumberPolicy accountNumberPolicy;
 
     public CreateAccountUseCaseAdapter(AccountRepository accountRepository) {
         this.accountRepository = accountRepository;
         this.accountFactory = new AccountFactory();
+        this.accountNumberPolicy = new AccountNumberPolicy();
     }
 
     @Override
     public Account execute(CreateAccountCommand command) {
-        String number = "1223131232132131232";
+        String number = accountNumberPolicy.generate();
 
         Account account = accountFactory.create(
                 number,
